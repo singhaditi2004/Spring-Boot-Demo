cf0664df63b1931782206f1bfca7e2c1003b2182
Aditi Singh <aditisinghbais_cse21@ggct.co.in>
2024-09-12 14:58:50 +0530

    Added packages

--- src/main/java/com/springboot/first/springboot_demo/Controller.java
+++ src/main/java/com/springboot/first/springboot_demo/Controller.java
@@ -1,8 +1,18 @@
 package com.springboot.first.springboot_demo;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.springboot.first.springboot_demo.entity.Department;
 
 @RestController
 public class Controller {
+	@PostMapping("/departments")
+	public Department saveDepartment(@RequestBody Department department) {
+		
+		return department;
+		
+	}
 
 }
