b76aee3e452457e71d0a12a0115fc1b89959e42b
Aditi Singh <aditisinghbais_cse21@ggct.co.in>
2024-09-17 21:02:20 +0530

    Validation added

--- src/main/java/com/springboot/first/springboot_demo/Controller.java
+++ src/main/java/com/springboot/first/springboot_demo/Controller.java
@@ -15,13 +15,15 @@ import com.springboot.first.springboot_demo.entity.Department;
 import com.springboot.first.springboot_demo.service.DepartmentSeriveImplementation;
 import com.springboot.first.springboot_demo.service.DepartmentService;
 
+import jakarta.validation.Valid;
+
 @RestController
 public class Controller {
 	@Autowired
 	private DepartmentService departmentService;
 
 	@PostMapping("/departments")
-	public Department saveDepartment(@RequestBody Department department) {
+	public Department saveDepartment(@Valid @RequestBody Department department) {
 
 		return departmentService.saveDepartment(department);
 
--- src/main/java/com/springboot/first/springboot_demo/entity/Department.java
+++ src/main/java/com/springboot/first/springboot_demo/entity/Department.java
@@ -4,12 +4,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
 
 @Entity
 public class Department {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long departmentId;
+	@NotBlank(message= "Please add Department name")
 	private String departmentName;
 	private String dAdd;
 	private String departmentCode;
