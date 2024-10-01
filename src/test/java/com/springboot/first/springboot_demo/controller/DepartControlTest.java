2f923e1e91f05a002b8390b4279ecb7939ab03a9
Aditi Singh <aditisinghbais_cse21@ggct.co.in>
2024-10-01 13:48:19 +0530

    contrller test

---
+++ src/test/java/com/springboot/first/springboot_demo/controller/DepartControlTest.java
@@ -0,0 +1,19 @@
+package com.springboot.first.springboot_demo.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class DepartControlTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
