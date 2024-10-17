d5460200a45ade5587691a83a6d15f8d58b95fc8
Aditi Singh <aditisinghbais_cse21@ggct.co.in>
2024-10-17 22:30:17 +0530

    added mock

--- src/test/java/com/springboot/first/springboot_demo/controller/DepartControlTest.java
+++ src/test/java/com/springboot/first/springboot_demo/controller/DepartControlTest.java
@@ -4,16 +4,57 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import com.springboot.first.springboot_demo.entity.Department;
+import com.springboot.first.springboot_demo.service.DepartmentService;
+
+@WebMvcTest(DepartControlTest.class)
 class DepartControlTest {
-
+	@Autowired
+	private MockMvc mockMVC;
+	@MockBean
+	private DepartmentService departSEr;
+    private Department depart;
+    
 	@BeforeEach
-	void setUp() throws Exception {
+	void setUp() {
+		depart=Department.builder()
+				.dAdd("DELHI")
+				.departmentCode("IT-56")
+				.departmentId(12L)
+				.departmentName("IT")
+				.build();
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void saveDepartment() throws Exception {
+		Department inpdepart=Department.builder()
+				.dAdd("Bombay")
+				.departmentCode("IT-56")
+				.departmentId(12L)
+				.departmentName("IT")
+				.build();
+		Mockito.when(departSEr.saveDepartment(inpdepart)).thenReturn(depart);
+		mockMVC.perform(MockMvcRequestBuilders.post("")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content("{\n" +
+						"\t\"departmentName\":\"IT\",\n" +
+						"\t\"departmentAddress\":\"Ahmedabad\", \n" +
+						"\t\"departmentCode\":\"IT-06\"\n" +
+						"}"))
+						.andExpect(MockMvcResultMatchers.status().isOk());
 	}
 
+	@Test
+	void fetchDepartById() {
+		
+	}
 }
